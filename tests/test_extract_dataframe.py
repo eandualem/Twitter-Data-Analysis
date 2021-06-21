12806175757af9c9b128fc6ef9867d22d3965840
Elias <eandualem@gmail.com>
2021-06-21 22:49:18 +0300

    test: add test for find_hashtags

--- tests/test_extract_dataframe.py
+++ tests/test_extract_dataframe.py
@@ -78,8 +78,11 @@ class TestTweetDfExtractor(unittest.TestCase):
     def test_find_retweet_count(self):
         self.assertEqual(self.df.find_retweet_count(), [612, 92, 1, 899, 20])
 
-    # def test_find_hashtags(self):
-    #     self.assertEqual(self.df.find_hashtags(), )
+   
+   def test_find_hashtags(self):
+        self.assertEqual(self.df.find_hashtags(), [
+                         [], [], [{'text': 'red4research', 'indices': [103, 116]}], [], []])
+
 
     # def test_find_mentions(self):
     #     self.assertEqual(self.df.find_mentions(), )
